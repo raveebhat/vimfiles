d79c15d0fd479eb691ff717dc788cc1131b95a64
ajy <ajygvnkr@gmail.com>
2012-09-02 17:17:45 +0530

    added a bootstrapping script

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cp -r ~/.vim ~/.vim.old 2>/dev/null
+rm -fr ~/.vim 2>/dev/null
+cp ~/.vimrc ~/.vimrc_backup 2>/dev/null
+rm -f ~/.vimrc 2>/dev/null
+git clone https://github.com/ajy/vimfiles ~/.vim && cd ~/.vim && ln -s ~/.vim/vimrc ~/.vimrc
+#for tagbar
+sudo apt-get install exuberant-ctags
