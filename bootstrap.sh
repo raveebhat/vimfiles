1bc12c9dfd8bd8ca6e27c6fdc4a7d62b080c7be7
ajy <ajygvnkr@gmail.com>
2012-09-02 17:57:10 +0530

    forgot to bootstrap vundle

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,7 @@ rm -fr ~/.vim 2>/dev/null
 cp ~/.vimrc ~/.vimrc_backup 2>/dev/null
 rm -f ~/.vimrc 2>/dev/null
 git clone https://github.com/ajy/vimfiles ~/.vim && cd ~/.vim && ln -s ~/.vim/vimrc ~/.vimrc
+# for getting vundle
+git submodule init && git submodule update
 #for tagbar
 sudo apt-get install exuberant-ctags
