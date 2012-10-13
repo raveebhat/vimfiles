d2ac9ae5f44266bf8c3a7f15b8e286410bc9e100
ajy <ajygvnkr@gmail.com>
2012-10-13 19:27:11 +0530

    tweaked the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#backup old config
 cp -r ~/.vim ~/.vim.old 2>/dev/null
 rm -fr ~/.vim 2>/dev/null
 cp ~/.vimrc ~/.vimrc_backup 2>/dev/null
@@ -9,4 +10,4 @@ git submodule init && git submodule update
 #for tagbar
 sudo apt-get install exuberant-ctags
 # now install plugins into vim
-vim +BundleInstall +q +q
+vim +BundleInstall +qall
