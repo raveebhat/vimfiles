1f71ec46dc107634301766af337d1c677d44bcf6
ajy <ajygvnkr@gmail.com>
2012-12-24 22:31:53 +0530

    removing the vundle submodule dependency in favour of the actual bundle

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,19 @@ cp -r ~/.vim ~/.vim.old 2>/dev/null
 rm -fr ~/.vim 2>/dev/null
 cp ~/.vimrc ~/.vimrc_backup 2>/dev/null
 rm -f ~/.vimrc 2>/dev/null
+
+#retrieve my files, and setup it up
 git clone https://github.com/ajy/vimfiles ~/.vim && cd ~/.vim && ln -s ~/.vim/vimrc ~/.vimrc
-# for getting vundle
-git submodule init && git submodule update
-#for tagbar
+
+#create backup and swap directories
+mkdir ~/.vim/tmp
+mkdir ~/.vim/swp
+
+# get vundle first
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+#get tagbar
 sudo apt-get install exuberant-ctags
+
 # now install plugins into vim
-vim +BundleInstall +qall
+vim +BundleInstall
